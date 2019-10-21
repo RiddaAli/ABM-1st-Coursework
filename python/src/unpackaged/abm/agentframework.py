efd6276c65ce395d9fa5c18e6d00341da1bcb5c3
Ridda <ridda.ali@hotmail.com>
2019-10-21 21:28:25 +0100

    Unit test

--- python/src/unpackaged/abm/agentframework.py
+++ python/src/unpackaged/abm/agentframework.py
@@ -37,29 +37,35 @@ class Agent():
             
  
     def set_y(self, y):
-        """ Setters: Making y private """
+        """ Setters: Making y private and setting it equal to a specified 
+        integer value. """
         self._y = y
         
     def set_x(self, x):
-        """ Setters: Making x private """
+        """ Setters: Making x private setting it equal to a specified 
+        integer value. """
         self._x = x
         
     
     def get_y(self):
-        """ Getters: because y is private """
+        """ Getters: getting the private y value """
         return self._y
     
     def get_x(self):
-        """ Getters: because x is private """
+        """ Getters: getting the private x value """
         return self._x
     
 
     def move(self):
-        """ Moving agents randomly:
+        """ 
+        Moving agents randomly:
             - Problem: agents might be missing from the graph if they ended up
               wandering off the edge.
             - Solution: Torus - permit agents leaving the top of an area to
               come in at the bottom and leaving left come in on the right side.
+              
+        :param [self]: [pointing to the object itself ((data type: tuple))]
+        :return: [new random x and y values for the parameter self]
         """
         if random.random() < 0.5:
             self._y = (self._y + 1) % 300
@@ -70,13 +76,18 @@ class Agent():
             self._x = (self._x + 1) % 300
         else:
             self._x = (self._x - 1) % 300
-            
+    
+        
     def eat(self): 
         """ 
         Using the if statement to determine whether agents should eat the
         environment or not. 
         If the environment is greater than 10 than subtract 10 units from the 
         environment and add it to the agent's store.
+        
+        :param [self]: [pointing to the object itself (data type: tuple)]
+        :return: [values for the "environment" and "store" variables]
+
         """
         if self.environment[self.get_y()][self.get_x()] > 10:
             self.environment[self.get_y()][self.get_x()] -= 10
@@ -94,20 +105,23 @@ class Agent():
 # 
 # =============================================================================
 
-# =============================================================================
-#  Calculating the distance to each of the other agent and if they tend to be
-# within the neighbourhood distance and it sets the its and its neighbours 
-# stores equal to the average of their two stores
-# =============================================================================
-             
+            
     def share_with_neighbours(self, neighbourhood):
         """ 
+        Calculating the distance to each of the other agent,
+        and if they tend to be within the neighbourhood distance then sets its 
+        and its neighbours stores equal to the average of their two stores
+        
         Looping through the agents in self.agents.
         1) Calculating the distance between self and the current other agent.
-        2) If the distance is less than or equal to neighbourhood then add 
+        2) If the distance is less than or equal to neighbourhood 
+           (by default 20 is the radius around agents)then add 
            self.store and agent.store.
         3) Calculate the average by dividing the sum by 2. 
         4) Store this average into both the self.store and agent.store.
+    
+        :param [self, neighbourhood]: [pointing to the agent itself 
+        (data type: tuple) and neighbourhood (data type: integer)]
         """
         for agent in self.agents:
             distance = self.distance_between(agent)
@@ -119,13 +133,18 @@ class Agent():
                 
             
                 
-# Calculating how far the agents are by using Pythagoras
+
     def distance_between(self, other_agent):
             """
+            # Calculating how far the agents are by using Pythagoras
+            
             Agent 1 = self.
             Agent 2 = other_agent.
-            Calculating the distance between Agent 1 and Agent 2 by using 
-            Pythagoras.
+            
+            :param [self, other_agent]: [pointing to the agent itself 
+            (data type: tuple) and another agent (data type: tuple)]
+            :return: [distance between Agent 1 and Agent 2 by using 
+            Pythagoras (data type: float)]
             """
             return (((self.get_x() - other_agent.get_x())**2) +
                 ((self.get_y() - other_agent.get_y())**2))**0.5
--- python/src/unpackaged/abm/test_distance_between.py
+++ python/src/unpackaged/abm/test_distance_between.py
@@ -6,21 +6,30 @@ Created on Mon Oct 14 17:13:19 2019
 @author: gyral
 """
 
-import pytest
+import unittest
 import agentframework
 
+class TestDocs(unittest.TestCase):
+
+    def test_distance_between(self):
+        environment = [[2, 2],[ 2, 2]]
+        
+        agents_list = []
+        agents_list.append(agentframework.Agent(environment,agents_list,0, 0))
+        agents_list.append(agentframework.Agent(environment,agents_list, 0, 1))
+        
+        assert agents_list[0].distance_between(agents_list[1]) == 1, "There is a distance!"
+        assert agents_list[1].distance_between(agents_list[1]) == 0, "There is no distance!"
+        
+# =============================================================================
+# ----------------------------------------------------------------------
+# Ran 1 test in 0.001s
+# 
+# OK
+# =============================================================================
 
 
-def testing_distance_between():
-    environment = [[2, 2],[ 2, 2]]
+if __name__ == '__main__':
+    unittest.main()
     
-    agents_list = []
-    agents_list.append(agentframework.Agent(environment,agents_list,0, 0))
-    agents_list.append(agentframework.Agent(environment,agents_list, 0, 1))
-    
-    assert agents_list[0].distance_between(agents_list[1]) == 1, "There is a distance!"
-    assert agents_list[1].distance_between(agents_list[1]) == 0, "There is no distance!"
 
-
-pytest.main()
-testing_distance_between()
